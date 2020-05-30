35c84adfffe68d319d821c8d3b125f0b79c3ca17
mounir2KH <it_khodja@esi.dz>
2020-05-30 15:36:27 +0100

    destinationScreen: Activities / Logo added

--- lib/screens/destination_screen.dart
+++ lib/screens/destination_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/models/activity_model.dart';
 import 'package:flutter_travel_ui_starter/models/destination_model.dart';
-import 'package:flutter_travel_ui_starter/widgets/destination_carousel.dart';
-import 'package:flutter_travel_ui_starter/widgets/hotel_carousel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DestinationScreen extends StatefulWidget {
@@ -12,6 +11,15 @@ class DestinationScreen extends StatefulWidget {
 
 }
 class _DestinationScreenState extends State<DestinationScreen>{
+ Text  _buildRatingStars(int rating){
+    String stars ='' ;
+    for(int i =0;i< rating; i++)
+      {
+        stars += '⭐ ' ;
+      }
+    stars.trim();
+    return Text(stars) ;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -114,6 +122,116 @@ class _DestinationScreenState extends State<DestinationScreen>{
               ),
             )
             ],
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.only(top: 10.0, bottom: 15.0),
+              itemCount: widget.destination.activities.length,
+              itemBuilder: (BuildContext context, int index){
+                Activity activity = widget.destination.activities[index];
+                return Stack(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+                      height: 170.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Padding(
+                        padding:  EdgeInsets.fromLTRB(100.0,20.0,20.0,20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                width: 120.0,
+                                  child: Text(
+                                    activity.name,
+                                    style: TextStyle(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.w600,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                  )
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    '\$${activity.price}',
+                                    style: TextStyle(
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.w600
+                                    ),
+                                  ),
+                                  Text(
+                                    'per pax',
+                                    style: TextStyle(
+                                        color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                         ),
+                            Text(activity.type,style: TextStyle(color: Colors.grey),),
+                            _buildRatingStars(activity.rating),
+                            SizedBox(height: 10.0,),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Row(children: <Widget>[
+                                Container(
+
+                                  width: 70.0,
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).accentColor,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Text(activity.startTimes[0],textAlign: TextAlign.center,),
+                                ),
+                                SizedBox(width: 10.0),
+                                Container(
+
+                                  width: 70.0,
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).accentColor,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Text(activity.startTimes[1],textAlign: TextAlign.center,),
+                                ),
+                              ],
+                              ),
+                            )
+                        ],),
+                      ),
+                    ),
+                    Positioned(
+                      left: 20.0,
+                      top: 15.0,
+                      bottom: 15.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                      child: Image(
+                        width: 110.0,
+                        image: AssetImage(
+                            activity.imageUrl,
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                      ),
+                    ),
+                ],
+                );
+              },
+              ),
         ),
       ],
         )
